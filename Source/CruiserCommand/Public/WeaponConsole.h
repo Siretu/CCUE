83fc80be2569f5e2baaf005cf8393c0d5c9bca05
Siretu <erikihr@gmail.com>
2015-07-27 22:27:55 +0200

    Added stubs for weapon console

---
+++ Source/CruiserCommand/Public/WeaponConsole.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GenericConsole.h"
+#include "WeaponConsole.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUISERCOMMAND_API AWeaponConsole : public AGenericConsole
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
